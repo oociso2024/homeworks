83396f01f89babcb20467079294a7e31170e87de
Oleg Nanovsky <oociso@ya.ru>
2024-07-07 00:16:52 +0700

    функции первое задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,21 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = - Infinity;
+  let sum = arr.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+  });
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min){
+      min = arr[i];
+    }
+    if (arr[i] > max){
+      max = arr[i];
+    }
+  }
+  let avg = Number((sum / arr.length).toFixed(2));
 
-  return { min: min, max: max, avg: avg };
-}
+ return { min: min, max: max, avg: avg };
+}  
 
 function summElementsWorker(...arr) {
 
