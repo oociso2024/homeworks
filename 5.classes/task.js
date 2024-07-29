d4f745cfb65353905f0a15bacf93bc9da6cb2bfb
Oleg Nanovsky <oociso@ya.ru>
2024-07-30 02:53:07 +0700

    третья задача пятого задания

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -92,5 +92,48 @@ class Library {
     }
 };
 
-
-
+class Student {
+    constructor(name) {
+      this.name = name;
+      this.marks = {};
+    }
+    addMark(mark, subject) {
+        if (this.marks?.[subject] === undefined && mark >= 2 && mark <= 5) {
+        this.marks[subject] = [];
+        this.marks[subject].push(mark);
+      } else if (this.marks.hasOwnProperty(subject) && mark >= 2 && mark <= 5) {
+        this.marks[subject].push(mark);
+      }
+    }
+    getAverageBySubject(subject) {
+      if (this.marks.hasOwnProperty(subject) === false) {
+        return 0;
+      }
+      const gradePointAverage = this.marks[subject].reduce((acc, mark, index, arr) => {
+        acc += mark;
+        if (index === arr.length - 1) {
+          return acc / arr.length;
+        }
+        return acc;
+      }, 0);
+      return gradePointAverage;
+    }
+    getAverage() {
+      if (Object.keys(this.marks).length === 0) {
+        return 0;
+      }
+      let allSubject = Object.keys(this.marks);
+      for (let subject of allSubject) {
+        allSubject[allSubject.indexOf(subject)] = this.getAverageBySubject(subject);
+      }
+      const gradePointAverage = allSubject.reduce((acc, mark, index, arr) => {
+        acc += mark;
+        if (index === arr.length - 1) {
+            return acc / arr.length;
+        }
+        return acc;
+        }, 0);
+        return gradePointAverage;
+    }
+};
+  
